e2e72c28ab1649ed138dc106c8cda9ae5ea376e5
tseegii <tseegii.g@gmail.com>
2013-06-24 19:03:16 +0800

    adadasasdasd

--- src/controller/Test.java
+++ src/controller/Test.java
@@ -8,6 +8,6 @@ package controller;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("args = " + args);
+        System.out.println("dsfdsfsdfsd");
     }
 }
