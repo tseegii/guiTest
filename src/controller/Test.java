43a31a01cb3fd30fd5d43956e4c6d080a4855f00
tseegii <tseegii.g@gmail.com>
2013-06-24 17:55:28 +0800

    adadasasdasd

---
+++ src/controller/Test.java
@@ -0,0 +1,13 @@
+package controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tseegii
+ * Date: 6/24/13
+ * Time: 5:55 PM
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("args = " + args);
+    }
+}
